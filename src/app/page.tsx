bf0ce9caba11ce986b418d893bdec0b0ff45bec6
JP <679434+npgenx@users.noreply.github.com>
2024-11-25 14:05:47 -0800

    tweak home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
+'use client';
+
 import { ModeToggle } from '@/components/ModeToggle';
+import { useTheme } from 'next-themes';
 import Image from 'next/image';
+import { useWeatherContext } from '@/providers/weather-provider';
 
 export default function Home() {
+    const { theme } = useTheme();
+    const { a } = useWeatherContext();
     return (
         <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
             <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
                 <ModeToggle />
+                The value is : {a}
                 <div>
                     <Image
-                        className='light:invert'
-                        src='./vercel.svg'
+                        className={theme === 'dark' ? 'invert': ':light:invert'}
+                        src='./images/01n@2x.png'
                         alt='Vercel logomark'
                         width={200}
                         height={200}
