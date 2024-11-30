8a91e02c68a8b3a70c779cc152a635301d726c5c
JP Morgan <679434+npgenx@users.noreply.github.com>
2024-11-30 09:37:37 -0800

    updated Humidity card

--- src/components/Humidity.tsx
+++ src/components/Humidity.tsx
@@ -1,24 +1,28 @@
 import {
     Card,
     CardContent,
-    CardDescription,
     CardFooter,
     CardHeader,
     CardTitle,
 } from '@/components/ui/card';
+import {Droplets} from 'lucide-react';
 
 import React from 'react';
 
 const Humidity = () => {
     return (
-        <Card className='col-span-2'>
+        <Card className='col-span-2 flex flex-col'>
             <CardHeader>
-                <CardTitle> Humidity</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+                <CardTitle className='flex items-center gap-3'>
+                    <Droplets />
+                    Humidity
+                </CardTitle>
             </CardHeader>
-            <CardContent>Card Content</CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
+            <CardContent className='flex grow items-center justify-center'>
+                <p className='text-5xl font-bold'>55%</p>
+            </CardContent>
+            <CardFooter className=''>
+                <p>Dry: May cause skin irritation</p>
             </CardFooter>
         </Card>
     );
