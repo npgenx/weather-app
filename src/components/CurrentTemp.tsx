315ea4d7cbca94fcab90db7b126d33614ec0ff6c
JP Morgan <679434+npgenx@users.noreply.github.com>
2024-12-02 21:28:10 -0800

    fixed edge case with night being Clear instead of Sunny

--- src/components/CurrentTemp.tsx
+++ src/components/CurrentTemp.tsx
@@ -27,6 +27,18 @@ const CurrentTemp = () => {
     const {currentWeather, dailyWeather} = useWeatherContext();
 
     if (!currentWeather || !dailyWeather) return <CTSkeleton />;
+
+    const iconSource = (currentWeather.weather_code == 0 && currentWeather.is_day == 0) ?
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMCIgaGVpZ2h0PSIyMCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIwLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtbW9vbiI+PHBhdGggZD0iTTEyIDNhNiA2IDAgMCAwIDkgOSA5IDkgMCAxIDEtOS05WiIvPjwvc3ZnPg=='
+        : `./images/${getWMOInfo(currentWeather.weather_code)?.icon}${(currentWeather.is_day) ? 'd' : 'n'}@2x.png`;
+    
+    const weatherDescription = (currentWeather.weather_code == 0 && currentWeather.is_day == 0) ? "Clear" : getWMOInfo(currentWeather.weather_code)?.text
+    
+    
+                    
+
+
+
     return (
         <Card className='col-span-4 lg:col-span-3 lg:row-span-2 text-center min-h-[600px] flex flex-col'>
             <CardHeader className='p-10'>
@@ -57,16 +69,15 @@ const CurrentTemp = () => {
             <CardContent className='flex flex-col justify-center p-0'>
                 <Image
                     className=' mydropshadow self-center'
-                    src={`./images/${
-                        getWMOInfo(currentWeather.weather_code)?.icon
-                    }d@2x.png`}
+                    // src={`./images/${
+                    //     getWMOInfo(currentWeather.weather_code)?.icon
+                    // }d@2x.png`}
+                    src={iconSource}
                     alt='weather'
                     width={180}
                     height={180}
                 />
-                <p className='font-bold p--0'>
-                    {getWMOInfo(currentWeather.weather_code)?.text}
-                </p>
+                <p className='font-bold p--0'>{weatherDescription}</p>
             </CardContent>
             <CardFooter className=' flex flex-col'>
                 <div className='flex p-4 justify-around gap-2 w-full text-xl font-semibold'>
